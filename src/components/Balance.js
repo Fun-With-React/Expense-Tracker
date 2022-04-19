e6485d1852616b9f4781115790efb7e321afc183
jamaaldev <kabarojamaal@gmail.com>
2022-04-19 23:52:22 +0100

    added:textbox display the amount

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,13 +4,12 @@ import { Card } from "react-bootstrap";
 const Balance = () => {
   return (
     <>
-     
-        <Card className='balance'>
+      <Card className="balance">
+        <div className="d-flex gap-3">
           <h3>Balance:</h3>
-        </Card>
-       
-       
-        
+          <p className="mt-2">$2,000.00</p>
+        </div>
+      </Card>
     </>
   );
 };
--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import {Row,Col} from 'react-bootstrap'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const IncomeExpense = () => {
   return (
     <>
-     <Row style={{padding: '100px'}}>
-            <Col md ={6}>
-                <h4>Expenses:</h4>
-            </Col>
-            <Col md ={6}>
-               <h4> Income:</h4>
-            </Col>
-        </Row>
+      <Row style={{ padding: "100px" }}>
+        <Col md={6}>
+          <div className="d-flex gap-3">
+            <h4>Expenses:</h4>
+            <p className="mt-1">$100.00</p>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div className="d-flex gap-3">
+            <h4>Income:</h4>
+            <p className="mt-1">$1,500.00</p>
+          </div>
+        </Col>
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default IncomeExpense
+export default IncomeExpense;
