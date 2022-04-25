a603ac5b8339d4bdbd69416e7493c6082aa32140
Purnima-1 <baranwal.purnimagmail.com>
2022-04-25 13:21:14 -0700

    ccc

--- src/reducers/amountReducer.js
+++ src/reducers/amountReducer.js
@@ -5,8 +5,8 @@ export const expenseSlice = createSlice({
     expenses: 0,
     income: 0,
     balance: 0,
-    listExpense: localStorage.getItem('listExpense')? JSON.parse(localStorage.getItem('listExpense')) : [],
-    listIncome: localStorage.getItem('listIncome')? JSON.parse(localStorage.getItem('listIncome')) : []
+    listExpense: localStorage.getItem("listExpense") ? JSON.parse(localStorage.getItem("listExpense")) : [],
+    listIncome: localStorage.getItem("listIncome") ? JSON.parse(localStorage.getItem("listIncome")) : [],
   },
   reducers: {
     addExpense: (state, action) => {
@@ -20,22 +20,27 @@ export const expenseSlice = createSlice({
     },
     addListExpense: (state, action) => {
       state.listExpense.push(action.payload);
-      localStorage.setItem('listExpense',JSON.stringify(state.listExpense))
+      localStorage.setItem("listExpense", JSON.stringify(state.listExpense));
     },
-    addListIncome:(state,action) => {
-      state.listIncome.push(action.payload)
-      localStorage.setItem('listIncome',JSON.stringify(state.listIncome))
+    addListIncome: (state, action) => {
+      state.listIncome.push(action.payload);
+      localStorage.setItem("listIncome", JSON.stringify(state.listIncome));
     },
-    deleteExpenseFromLocalStorage:(state,action) => {
-      const newList = JSON.parse(localStorage.getItem('listExpense'))
-      state.listExpense = newList.filter((each)=> {
-        return each.id !== action.payload
-              })
-      localStorage.setItem('listExpense', JSON.stringify(state.listExpense))
+    deleteExpenseFromLocalStorage: (state, action) => {
+      const newList = JSON.parse(localStorage.getItem("listExpense"));
+      state.listExpense = newList.filter((each) => {
+        return each.id !== action.payload;
+      });
+      localStorage.setItem("listExpense", JSON.stringify(state.listExpense));
     },
-   
+    deleteIncomeFromLocalStorage: (state, action) => {
+      const newList = JSON.parse(localStorage.getItem("listIncome"));
+      state.listIncome = newList.filter((each) => {
+        return each.id !== action.payload;
+      });
+    },
   },
 });
 
-export const { addExpense, addIncome, addListExpense, addListIncome,deleteExpenseFromLocalStorage,deleteIncomeFromLocalStorage } = expenseSlice.actions;
+export const { addExpense, addIncome, addListExpense, addListIncome, deleteExpenseFromLocalStorage, deleteIncomeFromLocalStorage } = expenseSlice.actions;
 export default expenseSlice.reducer;
