98f2928b977332066d49e672814a0b3dfc83ea62
jamaaldev <kabarojamaal@gmail.com>
2022-04-29 07:24:57 +0100

    added:func for filtering week,month,year

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
-//import { useSelector } from 'react-redux'
 import Balance from "./Balance";
 import Expense from "./Expense";
 import Income from "./Income";
@@ -9,7 +8,55 @@ import Income from "./Income";
 import TransactionList from "./TransactionList";
 
 const History = () => {
-  const { expenses, income ,listToHistory} = useSelector((state) => state.expenseSlice);
+  const { expenses, income, listToHistory } = useSelector((state) => state.expenseSlice);
+  const [list, setList] = useState(listToHistory);
+
+  let checkChange = [];
+  const getSelection = (selected) => {
+    checkChange.length = "";
+    if (!checkChange.length) {
+      checkChange.push(selected);
+    }
+  };
+
+  const dateFilter = (list) => {
+    let dayNumberList = new Date(list?.expense?.id || list?.income?.id).getDate();
+    let monthNumberList = new Date(list?.expense?.id || list?.income?.id).getMonth() + 1;
+    let yearNumberList = new Date(list?.expense?.id || list?.income?.id).getFullYear();
+
+    if (checkChange[0] === "week") {
+      if (new Date().getFullYear() === yearNumberList && new Date().getMonth() + 1 === monthNumberList && new Date().getDate() - 20 <= dayNumberList) {
+        return list;
+      }
+    }
+
+    if (checkChange[0] === "month") {
+      if ((new Date().getFullYear() === yearNumberList && new Date().getMonth() + 1 === monthNumberList) || new Date().getMonth() + 1 !== monthNumberList) {
+        return list;
+      }
+    }
+    if (checkChange[0] === "year") {
+      if (new Date().getFullYear() !== yearNumberList || new Date().getFullYear() === yearNumberList) {
+        return list;
+      }
+    }
+
+    return;
+  };
+  const selectList = (selected) => {
+    getSelection(selected);
+    if (selected === "week") {
+      setList(listToHistory.filter(dateFilter));
+    }
+
+    if (selected === "month") {
+      setList(listToHistory.filter(dateFilter));
+    }
+    if (selected === "year") {
+      setList(listToHistory.filter(dateFilter));
+    }
+  };
+
   return (
     <>
       <Balance />
@@ -24,17 +71,17 @@ const History = () => {
       <Container className="mx-auto d-flex justify-content-center bg-light w-100">
         <Row className="w-100 d-block ">
           <Col className="my-5" md={3}>
-            <Form.Select aria-label="Default select example">
+            <Form.Select onChange={(e) => selectList(e.target.value)} aria-label="Default select example">
               <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              <option value="week">Week</option>
+              <option value="month">Month</option>
+              <option value="year">Year</option>
             </Form.Select>
           </Col>
           <Col md={8}>
-          {listToHistory.map((historyList, index) => (
-            <TransactionList key={index} id={historyList?.expense?.id || historyList?.income?.id} money={`${historyList?.expense?.money || historyList?.income?.money}`} date={historyList?.expense?.currentTime || historyList?.income?.currentTime} text={historyList?.expense?.text || historyList?.income?.text} />
-          ))}
+            {list.map((historyList, index) => (
+              <TransactionList key={index} id={historyList?.income?.id || historyList?.expense?.id} money={historyList?.income?.money || historyList?.expense?.money} date={historyList?.income?.currentTime || historyList?.expense?.currentTime} text={historyList?.income?.text || historyList?.expense?.text} />
+            ))}
           </Col>
         </Row>
       </Container>
--- src/reducers/amountReducer.js
+++ src/reducers/amountReducer.js
@@ -7,8 +7,23 @@ export const expenseSlice = createSlice({
     balance: 0,
     listExpense: localStorage.getItem("listExpense") ? JSON.parse(localStorage.getItem("listExpense")) : [],
     listIncome: localStorage.getItem("listIncome") ? JSON.parse(localStorage.getItem("listIncome")) : [],
-    listToHistory: localStorage.getItem("listExpenseHistory") ? JSON.parse(localStorage.getItem("listExpenseHistory")) : [],
-      },
+    listToHistory: localStorage.getItem("listExpenseHistory")
+      ? JSON.parse(localStorage.getItem("listExpenseHistory"))
+      : localStorage.setItem(
+          "listExpenseHistory",
+          JSON.stringify([
+            { expense: { id: 1619582100000, text: "aa", money: "-$11", currentTime: "Apr 28, 2021" } },
+            { income: { id: 1619582102000, text: "aa", money: "$11", currentTime: "Apr 28, 2021" } },
+            { expense: { id: 1648439702000, text: "bb", money: "-$22", currentTime: "Mar 28, 2022" } },
+            { income: { id: 1648439703000, text: "bb", money: "$22", currentTime: "Mar 28, 2022" } },
+            { income: { id: 1649303703000, text: "cc", money: "$33", currentTime: "Apr 07, 2022" } },
+            { expense: { id: 1651108247885, text: "cc", money: "-$33", currentTime: "Apr 28, 2022" } },
+            { expense: { id: 1651062145000, text: "cc", money: "-$33", currentTime: "Apr 27, 2022" } },
+            { expense: { id: 1650889345000, text: "cc", money: "-$33", currentTime: "Apr 25, 2022" } },
+            { expense: { id: 1651234945000, text: "cc", money: "-$33", currentTime: "Apr 29, 2022" } },
+          ]),
+        ),
+  },
   reducers: {
     addExpense: (state, action) => {
       state.expenses += action.payload;
@@ -21,13 +36,13 @@ export const expenseSlice = createSlice({
     },
     addListExpense: (state, action) => {
       state.listExpense.push(action.payload);
-      state.listToHistory.push({expense:action.payload})
+      state.listToHistory.push({ expense: action.payload });
       localStorage.setItem("listExpense", JSON.stringify(state.listExpense));
       localStorage.setItem("listToHistory", JSON.stringify(state.listToHistory));
     },
     addListIncome: (state, action) => {
       state.listIncome.push(action.payload);
-      state.listToHistory.push({income:action.payload})
+      state.listToHistory.push({ income: action.payload });
       localStorage.setItem("listIncome", JSON.stringify(state.listIncome));
       localStorage.setItem("listToHistory", JSON.stringify(state.listToHistory));
     },
@@ -44,8 +59,7 @@ export const expenseSlice = createSlice({
         return each.id !== action.payload;
       });
       localStorage.setItem("listIncome", JSON.stringify(state.listIncome));
-    }
-    
+    },
   },
 });
 
