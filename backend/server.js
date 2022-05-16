7211bf9be0d23010ae34ccbee7b23db770c0ed9a
Purnima-1 <baranwal.purnimagmail.com>
2022-05-16 15:57:41 -0700

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require("path")
 const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
@@ -15,22 +14,16 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(userRoutes);
 
-//const __dirname = path.resolve()
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get("/", (req, res) => {
+  res.send("Hello expense tracker");
+});
 
-  app.get("*", (req,res) =>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-  } )
-} else{
-  app.get('/',(req,res) => {
-    res.send('API is running...')
-  })
-}
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`Server is running on ${PORT}`));
