8c206707a8ef8ef29f556de08f9df0f148d6ffaa
Purnima-1 <baranwal.purnimagmail.com>
2022-05-18 11:38:45 -0700

    deployed

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,8 +1,20 @@
 import Image from "react-bootstrap/Image";
+import { useSelector, useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
+import { logOut } from "../reducers/userReducer";
+import Login from "./Login";
 const Avatar = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.loginSlice);
+  const logOutHandler = () => {
+    dispatch(logOut());
+  };
   return (
     <>
-      <Image thumbnail={true} roundedCircle={true} src="https://www.stevensegallery.com/50/50" alt="profile image" />;
+      <Image className = 'mx-5'thumbnail={true} roundedCircle={true} src="https://www.stevensegallery.com/50/50" alt="profile image"/><br/>
+      {token ? <Button onClick={logOutHandler}> Logout </Button>  : <Login />}  
+        
+        
     </>
   );
 };
